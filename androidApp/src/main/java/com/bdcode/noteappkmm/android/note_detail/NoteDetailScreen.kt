2458ce35afc5dca7a60827c1155e0cfae8c67214
DevarshBhalara <devarshbhalara3072@gmail.com>
2024-07-01 22:35:05 +0530

    Initial Commit 2

--- androidApp/src/main/java/com/bdcode/noteappkmm/android/note_detail/NoteDetailScreen.kt
+++ androidApp/src/main/java/com/bdcode/noteappkmm/android/note_detail/NoteDetailScreen.kt
@@ -1,23 +1,32 @@
 package com.bdcode.noteappkmm.android.note_detail
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -40,20 +49,7 @@ fun NoteDetailScreen(
     }
 
 
-    Scaffold(
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = viewModel::saveNote,
-                backgroundColor = Color.Black,
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Check,
-                    contentDescription = "Save Note",
-                    tint = Color.White
-                )
-            }
-        }
-    ) { padding ->
+    Scaffold { padding ->
 
         Column(
             modifier = Modifier
@@ -62,6 +58,38 @@ fun NoteDetailScreen(
                 .padding(padding)
                 .padding(16.dp),
         ) {
+
+            Row (
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ){
+                IconButton(onClick = {
+                    navController.popBackStack()
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Back",
+                        tint = Color.Black,
+                        modifier = Modifier
+                            .padding(0.dp)
+                    )
+                }
+
+                IconButton(onClick = viewModel::saveNote
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Check,
+                        contentDescription = "Save",
+                        tint = Color.Black,
+                        modifier = Modifier
+                            .padding(0.dp)
+                    )
+                }
+
+            }
+
+            Spacer(Modifier.height(30.dp))
 
             TransparentTextFieldHint(
                 text = state.noteTitle,
@@ -72,7 +100,7 @@ fun NoteDetailScreen(
                     viewModel.onNoteTitleFocusedChange(it.isFocused)
                 },
                 singleLine = true,
-                textStyle = TextStyle(fontSize = 20.sp),
+                textStyle = TextStyle(fontSize = 28.sp, fontWeight = FontWeight.Bold),
             )
             Spacer(modifier = Modifier.height(16.dp))
 
